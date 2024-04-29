6137fec2b9bb70622e943aa14224bf2d045c2067
Diego Miranda <diegomo1224@gmail.com>
2024-04-29 16:18:20 -0300

    att

--- App.js
+++ App.js
@@ -2,13 +2,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Importando as páginas que aparecerão
-import HomePrincipal from "./src/pages/home";
-import Login from "./src/pages/login";
-import Cadastro from "./src/pages/cadastro";
-import Tarefas from "./src/pages/tarefasUsuario";
-import HomeUsuario from "./src/pages/homeuser";
-import HomeProfessor from "./src/pages/homeprofessor";
-import TarefasPrivadas from "./src/pages/TarefasPrivadas";
 
 import "react-native-get-random-values";
 import Routes from "./src/routes";
@@ -17,14 +10,8 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-
     <NavigationContainer>
-      
-        <Routes />
-      
+      <Routes />
     </NavigationContainer>
-
-
-
   );
 }
